f8a54c0dc638a2bf4a45992b5a756f271f6e9787
NoVaJeeno <jeeno.aring@gmail.com>
2026-05-31 20:16:59 +0200

    Fix: Umstellung auf static export für fehlerfreies Deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
 }
 module.exports = nextConfig
